f07f79232d19a89a569b8a9892345ae7328dc44c
Bryan Bosire <bryanbosire@gmail.com>
2022-06-06 00:25:42 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+const names = ["Charlie", "Samip", "Ali"];
+
+function writeCards(names) {
+  var results = [];   
+  for (let i = 0; i < names.length; i++) {
+    results.push(names[i]);
+  }
+  console.log(results);
+}
+var results = writeCards('Thankyou, ${names[i]}, for a wonderful birthday gift!');
+console.log(results);
+
+function countDown(){
+    let countdown = 11;
+    while (countdown > 0){
+        countdown--;
+        console.log(countdown);
+    }
+}
